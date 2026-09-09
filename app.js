b564f16904e55b4f58071895abb1618adb2087b9
Palak Tiwari <tiwaripalak006@gmail.com>
2026-09-09 10:26:51 +0530

    Update package files and listings

--- app.js
+++ app.js
@@ -63,10 +63,14 @@ app.get("/listings/:id/edit",async (req,res)=>{
 });
 
 
-//UPDATE ROUTE
-app.put("/listings/:id",async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+// UPDATE ROUTE
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+
+    console.log(req.body); // check karne ke liye
+
+    await Listing.findByIdAndUpdate(id, req.body.listing);
+
     res.redirect(`/listings/${id}`);
 });
 
